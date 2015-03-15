daf3fbb0eed1ae519df068e0fca92cd1b2ffc0f1
down1013 <331316152@qq.com>
2015-03-15 22:15:32 +0100

    version 1.1

---
+++ secondWorld/secondWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  secondWorld
+//
+//  Created by ysx on 15-3-15.
+//  Copyright (c) 2015年 ysx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ secondWorld/secondWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  secondWorld
+//
+//  Created by ysx on 15-3-15.
+//  Copyright (c) 2015年 ysx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+sqsqsqsqsdsdsd
+@end
+
